a1fb2646742d5facaef18abe487a2d32ea821ab2
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-09 16:35:11 -0300

    adicionada rota de cadastro do admin

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
+import AdministradorController from '../controllers/admin.controller';
 import EscolaController from '../controllers/escolas.controller';
 
 const router = express.Router();
+const adminController = new AdministradorController();
 const escolaController = new EscolaController();
 
 // Rota para criar uma nova escola
 router.post('/escolas', escolaController.criarEscola);
+
+
+// Rota para cadastrar admin
+router.post('/admins', adminController.criarAdministrador); 
+
 
 export default router;

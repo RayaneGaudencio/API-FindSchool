4dda020d4b4bd6fcda91c9a87f03222db23132df
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-09 17:16:11 -0300

    +2 erros adicionados

--- src/services/erros/erros-validacao-dados.ts
+++ src/services/erros/erros-validacao-dados.ts
@@ -4,4 +4,6 @@ export default class ErrosValidacao {
     static SenhaFraca = 'Senha não atende os requisitos.';
     static NomeVazio = 'Nome deve ser informado.';
     static EscolaExistente = 'Já existe uma escola com este email ou CNPJ.';
+    static AdminExistente = 'Já existe um administrador com este email ou CPF.';
+    static CPFInvalido = 'CPF inválido.';
 }

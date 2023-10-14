4a782422b12afacaf4f5120237f91534a6aab3ff
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-14 16:42:49 -0300

    adição das configurações do cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,21 @@ import * as express from 'express';
 const swaggerUI = require('swagger-ui-express')
 import swaggerSpec from './swaggerConfig';
 import router from './routes/routes';
+const cors = require('cors')
 
 const app = express();
 
 // Middleware para lidar com o corpo das requisições
 app.use(express.json());
+
+const corsOptions = {
+  origin: [ 'http://localhost:3001', 'http://localhost:3000' ], // permite qualquer origem
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, 
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api', router); 
 
